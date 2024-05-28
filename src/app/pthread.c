30ce340b8346a21974e834353586a68cd9d48adc
Petrigh <petrighlautaro@alu.ing.unlp.edu.ar>
2024-05-28 17:12:37 -0300

    fix pthreads

--- src/app/pthread.c
+++ src/app/pthread.c
@@ -4,8 +4,11 @@
 
 int* A;
 int* B;
+int* TempA;
+int* TempB;
 int N;
 int T;
+int numThread;
 unsigned long length;
 pthread_barrier_t* barreraMerge;
 pthread_barrier_t barreraCompare;
@@ -19,15 +22,15 @@ si solo corro el comando ./script.sh va a correr el comando en el front
 */
 
 double dwalltime();
-void* inicializar(void);
+void printArrays(int, int, int);
+int inicializar(void);
 void* finalizar(void);
-void* mergesort(int*, int, int, int);
-void* sort(int*, int, int);
+void* mergesort(int*, int*, int, int, int);
+void* sort(int*, int*, int, int);
 int compare(int, int);
 void* divideAndConquer(void*);
 
 int main(int argc, char* argv[]){
-
     if (atoi(argv[1]) <= 0){
         printf("El tamano del array debe de ser mayor a 1\n");
         return 1;
@@ -37,14 +40,14 @@ int main(int argc, char* argv[]){
         printf("El numero de procesos debe de ser par mayor a 0\n");
         return 1;
     }
-
     N = atoi(argv[1]); // 2^N
     T = atoi(argv[2]); //cantidad de threads   comment: revisar 8 o mas threads en cluster
     int threads_ids[T];
     pthread_t misThreads[T];
+    numThread = T/2; //una mitad para A[] y la otra para B[]
     length = (1<<N); //longitud arrays
-    inicializar(); //Inicializa los dos arreglos
-    
+    if(inicializar()) //Inicializa los dos arreglos
+        printf("Error al alocar memoria.\n");
     double timetick = dwalltime();
 
     for(int id=0;id<T;id++){
@@ -68,7 +71,7 @@ int main(int argc, char* argv[]){
 
 /*__________________COMPARE______________________*/
 int compare(int left, int right){
-    for(int i=left;i<=right;i++){
+    for(int i=left;i<right;i++){
         if(A[i] != B[i]){
             return 1;
         }
@@ -77,41 +80,37 @@ int compare(int left, int right){
 }
 
 /*_________________MERGE SORT___________________*/
-void* mergesort(int* array, int left, int right, int fase){
+void* mergesort(int* array, int* temp, int left, int right, int fase){
     int longitud = right-left;
     int floor, offset;
     for(offset=fase;offset<=longitud;offset=offset*2){ //arma paquetes de 2; 4; ...; n/2 elementos
         for(int floor=left;floor<right;floor=floor+offset){ //sorta el paquete
-            sort(array, floor, floor+offset);
+            sort(array, temp, floor, floor+offset);
         }
     }
 }
 
-void* sort(int* array, int left, int right){
+void* sort(int* array, int* Temp, int left, int right){
     int middle = ((right - left)/2) + left;
-    int i=left, j=middle, k=0;
-    int* temp = (int*)malloc(sizeof(int)*(right-left));
+    int i=left, j=middle, k=left;
     while(i<middle && j<right){
         if(array[i] <= array[j])
-            temp[k++] = array[i++];
+            Temp[k++] = array[i++];
         else
-            temp[k++] = array[j++];
+            Temp[k++] = array[j++];
     }
     while (i < middle)
-        temp[k++] = array[i++];
+        Temp[k++] = array[i++];
     while(j < right)
-        temp[k++] = array[j++];
-    k--;
-    while(k >= 0) {
-        array[left + k] = temp[k];
-        k--;
+        Temp[k++] = array[j++];
+    for(k=left;k<right;k++){
+        array[k]=Temp[k];
     }
 }
 
 /*__________SCHEDULER________________*/
 void* divideAndConquer(void *arg){
     int tid=*(int*)arg;
-    int numThread = T/2; //una mitad para A[] y la otra para B[]
     int parte = length/numThread;
     int inicio, limite;
     int fase=0;
@@ -119,10 +118,11 @@ void* divideAndConquer(void *arg){
     while(parte<=length){
         inicio = parte*((tid)%numThread);
         limite = parte*((tid)%numThread)+parte;
+
         if(tid<numThread){
-            mergesort(A,inicio,limite,offset);
+            mergesort(A, TempA, inicio,limite,offset);
         }else{
-            mergesort(B,inicio,limite,offset);
+            mergesort(B, TempB, inicio,limite,offset);
         }
         pthread_barrier_wait(&barreraMerge[(tid/(1<<(fase+1)))]);
         if (tid % (1 << (fase + 1)) != 0) {
@@ -132,13 +132,13 @@ void* divideAndConquer(void *arg){
         offset = parte;
         parte = parte<<1;
     }
-
     pthread_barrier_wait(&barreraCompare);
 
     parte=length/T;
     inicio = parte*tid;
-    limite = parte*tid+parte-1;
+    limite = parte*tid+parte;
     if(compare(inicio,limite)){
+
         pthread_mutex_lock(&compareMutex);
         compareResult = 1;
         pthread_mutex_unlock(&compareMutex);
@@ -147,28 +147,28 @@ void* divideAndConquer(void *arg){
 }
 
 /*_______________INITIALIZER_________________*/
-void* inicializar(void){
+int inicializar(void){
     int i;
     //alocamos matrices en heap
     A = (int*)malloc(sizeof(int)*(length));
     B = (int*)malloc(sizeof(int)*(length));
+    TempA = (int*)malloc(sizeof(int)*(length));
+    TempB = (int*)malloc(sizeof(int)*(length));
+
+    if (A == NULL || B == NULL || TempA == NULL || TempB == NULL) 
+        return 1;
     barreraMerge = (pthread_barrier_t*)malloc(sizeof(pthread_barrier_t)*(T/2));
     //inicializamos A
     for(i=0;i<length;i++){
-        A[i] = rand() % 1024;
+        A[i] = rand() % (length/2); //siempre habran repetidos
         B[length-i-1] = A[i]; //simulo desordenados
     }
-    //repetimos elementos (numero 10 => nuestra nota)
-    A[0]= 10;
-    A[length-1]= 10;
-    B[0]= 10;
-    B[length-1]= 10;
-
     for(i=0;i<(T/2);i++){
         pthread_barrier_init(&barreraMerge[i], NULL, 2);
     }
     pthread_barrier_init(&barreraCompare, NULL, T);
     pthread_mutex_init(&compareMutex, NULL);
+    return 0;
 }
 
 void* finalizar(void){
@@ -179,6 +179,8 @@ void* finalizar(void){
     }
     free(A);
     free(B);
+    free(TempA);
+    free(TempB);
 }
 
 /*_____________TIME______________*/
@@ -192,4 +194,27 @@ double dwalltime()
 	gettimeofday(&tv,NULL);
 	sec = tv.tv_sec + tv.tv_usec/1000000.0;
 	return sec;
+}
+
+void printArrays(int start, int end, int length){
+
+    printf("A[%d]={ ",length);
+    for(int i=start;i<end-1;i++)
+        printf("%d;",A[i]);
+    printf("%d }\n",A[end-1]);
+
+    printf("B[%d]={ ",length);
+    for(int i=start;i<end-1;i++)
+        printf("%d;",B[i]);
+    printf("%d }\n",B[end-1]);
+
+    printf("T[%d]={ ",length);
+    for(int i=start;i<end-1;i++)
+        printf("%d;",TempA[i]);
+    printf("%d }\n\n",TempA[end-1]);
+
+    printf("U[%d]={ ",length);
+    for(int i=start;i<end-1;i++)
+        printf("%d;",TempB[i]);
+    printf("%d }\n\n",TempB[end-1]);
 }
--- src/app/secuencial.c
+++ src/app/secuencial.c
@@ -3,7 +3,7 @@
 
 int* A;
 int* B;
-int* temp;
+int* Temp;
 int N;
 unsigned long length;
 
@@ -34,6 +34,7 @@ int main(int argc, char* argv[]){
     double timetick = dwalltime();
     
     mergesort(A,0,length,2);
+    mergesort(B,0,length,2);
     int compareResult = compare(0,length);
 
     printf("Tiempo de ejecucion: %fs \n", dwalltime() - timetick);
@@ -71,21 +72,20 @@ void* mergesort(int* array, int left, int right, int fase){
 
 void* sort(int* array, int left, int right){
     int middle = ((right - left)/2) + left;
-    int i=left, j=middle, k=0;
-    temp = (int*)realloc(temp,sizeof(int)*(right-left));
+    int i=left, j=middle, k=left;
     while(i<middle && j<right){
         if(array[i] <= array[j])
-            temp[k++] = array[i++];
+            Temp[k++] = array[i++];
         else
-            temp[k++] = array[j++];
+            Temp[k++] = array[j++];
     }
     while (i < middle)
-        temp[k++] = array[i++];
+        Temp[k++] = array[i++];
     while(j < right)
-        temp[k++] = array[j++];
+        Temp[k++] = array[j++];
     k--;
-    while(k >= 0) {
-        array[left + k] = temp[k];
+    while(k >= left) {
+        array[k] = Temp[k];
         k--;
     }
 }
@@ -97,7 +97,7 @@ void* inicializar(void){
     //alocamos matrices en heap
     A = (int*)malloc(sizeof(int)*(length));
     B = (int*)malloc(sizeof(int)*(length));
-    temp = (int*)malloc(sizeof(int)*(2));
+    Temp = (int*)malloc(sizeof(int)*(length));
     //inicializamos A
     for(i=0;i<length;i++){
         A[i] = rand() % 1024;
